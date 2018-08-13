ae8875e7570b55bee6f0220a840f70c5737bcf31
franco-extreme <franck_ral@hotmail.com>
2018-08-13 17:04:46 -0400

    Implement-ProjectBacklog-Component: Updated the routing and app.component.html file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectBacklogComponent } from './components/main/project-backlog/project-backlog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'project', component: ProjectBacklogComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
