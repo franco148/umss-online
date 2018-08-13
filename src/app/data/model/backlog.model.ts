2ea6b6ade95ad57ec6ecfcd061e79cd94d5e107f
franco-extreme <franck_ral@hotmail.com>
2018-08-13 17:00:57 -0400

    Implement-ProjectBacklog-Component: Implemented data model for project and backlog entities.

---
+++ src/app/data/model/backlog.model.ts
@@ -0,0 +1,11 @@
+export class Backlog {
+
+    private id: number;
+    private description: string;
+    private ammountOfUserStories: number;
+
+    private isDeleted: boolean;
+
+    private createdAt: Date;
+    private updatedAt: Date;
+}
---
+++ src/app/data/model/project.model.ts
@@ -0,0 +1,14 @@
+import { Backlog } from './backlog.model';
+export class Project {
+
+    private id: number;
+    private name: string;
+    private completedDateEstimation: Date;
+    private backlog: Backlog;
+
+    private isDeleted: boolean;
+
+    private createdAt: Date;
+    private updatedAt: Date;
+    // private
+}
