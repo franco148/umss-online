bb828542afcfcc60b4f990eee109255110bcedbe
franco-extreme <franck_ral@hotmail.com>
2018-08-15 21:52:58 -0400

    Implement-ProjectBacklog-Component: Implemented/defined models of the project..

--- src/app/data/model/task.model.ts
+++ src/app/data/model/task.model.ts
@@ -1,2 +1,15 @@
+// tslint:disable-next-line:quotemark
+import { User } from "./user.model";
+
 export class Task {
+    id: number;
+    name: string;
+    description: string;
+    priority: string;
+
+    assignedTo: User;
+    estimatedTime: number;
+
+    createdAt: Date;
+    updatedAt: Date;
 }
