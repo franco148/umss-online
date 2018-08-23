16d9121aa7200c00cc6e692c3784f13494bd7f0d
franco-extreme <franck_ral@hotmail.com>
2018-08-23 12:13:23 -0400

    Implement-InitialComponents: Implementation of uo-info-card component completed.

--- src/app/components/shared/uo-info-card/uo-info-card.component.ts
+++ src/app/components/shared/uo-info-card/uo-info-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UoInfoCardComponent implements OnInit {
 
-  isChangesInformation = false;
+  isChangesInformation = true;
   cardDescription = `The Shiba Inu is the smallest of the six original and
    distinct spitz breeds of dog from Japan. A small, agile dog that copes
    very well with mountainous terrain, the Shiba Inu was originally bred
