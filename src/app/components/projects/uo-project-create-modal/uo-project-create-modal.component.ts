bc19669e15710954a7242cced0a6f7e82c69a343
franco-extreme <franck_ral@hotmail.com>
2018-08-27 19:35:35 -0400

    Implement-UmssOnline-WelcomePage: Implemented Create Project Modal component.

--- src/app/components/projects/uo-project-create-modal/uo-project-create-modal.component.ts
+++ src/app/components/projects/uo-project-create-modal/uo-project-create-modal.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UoProjectCreateModalComponent implements OnInit {
 
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  minDate;
+
   constructor() { }
 
   ngOnInit() {
+    this.minDate = new Date();
+    this.minDate.setDate(this.minDate.getDate() + 1);
   }
 
+}
+// {
+//   "backlogDescription": "string",
+//   "completedDateEstimation": "dd-MM-yyyy",
+//   "createdById": 0,
+//   "name": "Google Maps REST Api"
+// }
+
+export interface Food {
+  value: string;
+  viewValue: string;
 }
