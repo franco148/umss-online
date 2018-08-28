b346a17920ae23881ee4aff301c49a9a604d12d5
franco-extreme <franck_ral@hotmail.com>
2018-08-27 23:18:01 -0400

    Implement-UmssOnline-WelcomePage: Initial implementation of Project Welcome component.

--- src/app/components/projects/uo-project-welcome/uo-project-welcome.component.ts
+++ src/app/components/projects/uo-project-welcome/uo-project-welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../../data/model/project.model';
 
 @Component({
   selector: 'app-uo-project-welcome',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UoProjectWelcomeComponent implements OnInit {
 
+  projectsList: Project[] = [];
+  projectName = 'Umss Online';
+
   constructor() { }
 
   ngOnInit() {
+    const p0 = new Project();
+    const p1 = new Project();
+    const p2 = new Project();
+
+    // this.projectsList.push(p0);
+    // this.projectsList.push(p1);
+    // this.projectsList.push(p2);
   }
 
 }
