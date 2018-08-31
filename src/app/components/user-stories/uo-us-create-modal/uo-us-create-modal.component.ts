f9be45dd8bf0ceafda4165c0de20408de1a366e5
franco-extreme <franck_ral@hotmail.com>
2018-08-31 00:06:08 -0400

    Implement-UmssOnline-UserStories: Create UserStoryCreateModal component.

---
+++ src/app/components/user-stories/uo-us-create-modal/uo-us-create-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uo-us-create-modal',
+  templateUrl: './uo-us-create-modal.component.html',
+  styleUrls: ['./uo-us-create-modal.component.css']
+})
+export class UoUsCreateModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
