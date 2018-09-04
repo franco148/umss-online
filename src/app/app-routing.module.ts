18470e4d479e0aa5f768d9ecd47f58e5fd706f37
franco-extreme <franck_ral@hotmail.com>
2018-09-04 09:07:38 -0400

    Implement-UmssOnline-UserStoryProcess: Implemented guard for verifying if any user is authenticated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { UoAuthLoginComponent } from './components/auth/uo-auth-login/uo-auth-login.component';
 import { UoAuthSignupComponent } from './components/auth/uo-auth-signup/uo-auth-signup.component';
 import { UoProjectWelcomeComponent } from './components/projects/uo-project-welcome/uo-project-welcome.component';
 import { UoProjectBacklogInfoComponent } from './components/projects/uo-project-backlog-info/uo-project-backlog-info.component';
+import { AuthGuard } from './components/auth/auth.guard';
 
 const routes: Routes = [
-    { path: '', component: UoProjectWelcomeComponent },
+    { path: '', component: UoProjectWelcomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: UoAuthLoginComponent },
     { path: 'signup', component: UoAuthSignupComponent },
-    { path: 'edit-profile/:id', component: UoAuthSignupComponent },
-    { path: 'project/:id', component: UoProjectBacklogInfoComponent }
+    { path: 'edit-profile/:id', component: UoAuthSignupComponent, canActivate: [AuthGuard] },
+    { path: 'project/:id', component: UoProjectBacklogInfoComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class AppRoutingModule { }
