4c6499e4fe026c90fa51c8dcc66a8724cd363a51
franco-extreme <franck_ral@hotmail.com>
2018-09-13 13:13:09 -0400

    Implement-UmssOnline-SprintProcess: Implementing UO Header component.

--- src/app/components/shared/uo-header/uo-header.component.ts
+++ src/app/components/shared/uo-header/uo-header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-uo-header',
   templateUrl: './uo-header.component.html',
   styleUrls: ['./uo-header.component.css']
 })
-export class UoHeaderComponent implements OnInit {
+export class UoHeaderComponent implements OnInit, OnDestroy {
+
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
 }
