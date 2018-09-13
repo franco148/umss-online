a63bd7c90c0e0b5851bac554582e1fa72f618032
franco-extreme <franck_ral@hotmail.com>
2018-09-13 16:17:52 -0400

    Implement-UmssOnline-SprintProcess: Implementing Sidenav Component.

--- src/app/components/shared/uo-sidenav/uo-sidenav.component.ts
+++ src/app/components/shared/uo-sidenav/uo-sidenav.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-uo-sidenav',
   templateUrl: './uo-sidenav.component.html',
   styleUrls: ['./uo-sidenav.component.css']
 })
-export class UoSidenavComponent implements OnInit {
+export class UoSidenavComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Output() sidenavOnClose = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChanged.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+
+    this.isAuth = this.authService.isAuthenticated();
   }
 
+  onClose() {
+    this.sidenavOnClose.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
 }
