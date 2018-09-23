bd8d9024a8382734267a13e35689643cc1953bcd
franco-extreme <franck_ral@hotmail.com>
2018-09-22 21:38:36 -0400

    Implement-UmssOnline-IntegrateDocViewerProcess: Applying clean code to the process.

--- src/app/components/documents/uo-doc-info/uo-doc-info.component.ts
+++ src/app/components/documents/uo-doc-info/uo-doc-info.component.ts
@@ -11,18 +11,9 @@ export class UoDocInfoComponent implements OnInit {
   constructor(private dmsService: DmsService) { }
 
   ngOnInit() {
-    // this.dmsService.findDocViewById('c99b8940-073d-45f7-88fc-3147365d597800-50-56-C0-00-08').subscribe(doc => {
-    //   //
-    // });
   }
 
   findDocById() {
-    // this.dmsService.findDocViewById('c99b8940-073d-45f7-88fc-3147365d597800-50-56-C0-00-08').subscribe(doc => {
-    //   console.log('Found a Document');
-    //   console.log(doc);
-    //   return doc;
-    // });
-
     return 'http://localhost:9090/api/v1/files/c99b8940-073d-45f7-88fc-3147365d597800-50-56-C0-00-08/view';
   }
 }
