b959c88ee36ecdb8c8ecbba6c09b0172c83dd5dd
franco-extreme <franck_ral@hotmail.com>
2018-11-07 22:55:17 -0400

    Implement-UmssOnline-DocumentVersions: Working on enabling and disabling the checkbox for selecting a file version.

--- src/app/components/shared/uo-info-card/uo-info-card.component.ts
+++ src/app/components/shared/uo-info-card/uo-info-card.component.ts
@@ -22,4 +22,8 @@ export class UoInfoCardComponent implements OnInit {
   onProjectSelect() {
     this.router.navigate(['/project', this.projectCardInfo.id]);
   }
+
+  onSelectVersion(event: Event) {
+    console.log(event);
+  }
 }
