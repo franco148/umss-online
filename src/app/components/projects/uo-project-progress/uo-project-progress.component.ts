37e6ac6d12149f8c9349ae01bfe70c3798ad9bb7
franco-extreme <franck_ral@hotmail.com>
2018-12-03 23:06:32 -0400

    UmssOnline-ProjectAnalytics: Implementing the table for the progress component.

--- src/app/components/projects/uo-project-progress/uo-project-progress.component.ts
+++ src/app/components/projects/uo-project-progress/uo-project-progress.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+
+import { UserStory } from '../../../data/model/user-story.model';
 
 @Component({
   selector: 'app-uo-project-progress',
   templateUrl: './uo-project-progress.component.html',
   styleUrls: ['./uo-project-progress.component.css']
 })
-export class UoProjectProgressComponent implements OnInit {
+export class UoProjectProgressComponent implements OnInit, AfterViewInit {
+
+  displayedColumns = ['id', 'name', 'priority', 'estimatedTime', 'assignedTo'];
+  dataSource = new MatTableDataSource<UserStory>();
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
