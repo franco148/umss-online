d3d6091e6a85b2d9f45c2b989eab1e30f522e459
franco-extreme <franck_ral@hotmail.com>
2019-01-20 16:59:12 -0400

    UmssOnline-ProjectAnalytics: Added a new component for displaying the projects' members.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { UoSprintInfoComponent } from './components/sprints/uo-sprint-info/uo-sp
 import { UoDocInfoComponent } from './components/documents/uo-doc-info/uo-doc-info.component';
 import { UoDocVersionsComponent } from './components/documents/uo-doc-versions/uo-doc-versions.component';
 import { UoActiveSprintComponent } from './components/sprints/uo-active-sprint/uo-active-sprint.component';
+import { SharingPanelComponent } from './components/members/sharing-panel/sharing-panel.component';
 
 const routes: Routes = [
     { path: '', component: UoProjectWelcomeComponent, canActivate: [AuthGuard] },
@@ -20,7 +21,8 @@ const routes: Routes = [
     { path: 'sprint', component: UoSprintInfoComponent, canActivate: [AuthGuard] },
     { path: 'project/:id/active-sprint', component: UoActiveSprintComponent, canActivate: [AuthGuard] },
     { path: 'project/:id/document', component: UoDocInfoComponent, canActivate: [AuthGuard] },
-    { path: 'project-document/:id/versions', component: UoDocVersionsComponent, canActivate: [AuthGuard] }
+    { path: 'project-document/:id/versions', component: UoDocVersionsComponent, canActivate: [AuthGuard] },
+    { path: 'members', component: SharingPanelComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
---
+++ src/app/components/members/sharing-panel/sharing-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sharing-panel',
+  templateUrl: './sharing-panel.component.html',
+  styleUrls: ['./sharing-panel.component.css']
+})
+export class SharingPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
