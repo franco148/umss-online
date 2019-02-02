73b2b0b542111e9c35c579e11481397a01dfce58
franco-extreme <franck_ral@hotmail.com>
2019-02-02 18:24:28 -0400

    UmssOnline-ProjectComments: Added a new component - Document notes modal.

---
+++ src/app/components/documents/uo-doc-notes-modal/uo-doc-notes-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uo-doc-notes-modal',
+  templateUrl: './uo-doc-notes-modal.component.html',
+  styles: []
+})
+export class UoDocNotesModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
