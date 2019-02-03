ad4b7af7d5e9eb9196a4294a22708768c0b4e3e2
franco-extreme <franck_ral@hotmail.com>
2019-02-03 19:25:54 -0400

    UmssOnline-ProjectComments: Project comments implemented and integrated with the Comments process.

--- src/app/components/documents/uo-docs-notes/uo-docs-notes.component.ts
+++ src/app/components/documents/uo-docs-notes/uo-docs-notes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NotesDto } from '../../../data/dto/notes-dto';
 
 @Component({
   selector: 'app-uo-docs-notes',
@@ -8,10 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class UoDocsNotesComponent implements OnInit {
 
   isChangesInformation = true;
-  cardDescription = `The Shiba Inu is the smallest of the six original and
-   distinct spitz breeds of dog from Japan. A small, agile dog that copes
-   very well with mountainous terrain, the Shiba Inu was originally bred
-   for hunting.`;
+  @Input() projectComment: NotesDto;
 
   constructor() { }
 
