46c60481d9b644d94a21675edab3389e94b1254e
franco-extreme <franck_ral@hotmail.com>
2020-02-06 00:35:49 -0400

    umss-online: Updated to display logged user information

--- src/app/components/shared/uo-sidenav/uo-sidenav.component.ts
+++ src/app/components/shared/uo-sidenav/uo-sidenav.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { AuthService } from '../../auth/auth.service';
 import { ProjectService } from '../../../service/project.service';
+import { User } from '../../../data/model/user.model';
 
 @Component({
   selector: 'app-uo-sidenav',
@@ -15,6 +16,7 @@ export class UoSidenavComponent implements OnInit, OnDestroy {
   @Output() sidenavOnClose = new EventEmitter<void>();
   isAuth = false;
   selectedProject = 0;
+  authUser: User = null;
 
   authSubscription: Subscription;
   projectSelectedSubscription: Subscription;
@@ -27,6 +29,7 @@ export class UoSidenavComponent implements OnInit, OnDestroy {
     });
 
     this.isAuth = this.authService.isAuthenticated();
+    this.authUser = this.authService.getUser();
 
     this.projectSelectedSubscription = this.projectService.projectSelectedChange.subscribe(selectedProject => {
       if (selectedProject) {
